ce4e504ef3967141ca8b7aab53fd4a833cd85aaa
Jack Clarke <me@jackclarke.xyz>
2020-03-27 14:03:51 +0000

    fix run.sh

--- run.sh
+++ run.sh
@@ -3,33 +3,39 @@
 echo "updating data and graphs"
 
 ./update_data.py
-[ -d "/path/to/dir" ] && ./generate_graphs.py & || ./generate_graphs.py
+if [ -d "./graphs" ]; then
+	echo "graphs dir exists"
+	./generate_graphs.py &
+else
+	echo "graphs dir does not exist"
+	./generate_graphs.py
+fi
 
 
 ./bot.py &
 
 
-i = 0
+i=0
 
 while true; do
 	# update every 5 hours
-	if [ "$i" = "18000" ]; do
+	if [ "$i" = "18000" ]; then
 		echo "updating data and graphs"
 
 		./update_data.py
 		./generate_graphs.py &
 
-		i = 0
-	done
+		i=0
+	fi
 
 
 	if [ $(( i % 3600 )) -eq 0 ]; then
-		hours_left = $(( ( 18000 - ( i / 3600 ) ) / 3600 ))
+		hours_left=$(( ( 18000 - ( i / 3600 ) ) / 3600 ))
 		echo "updating in $hours_left hours"
-	done
+	fi
 
 
-	i = $(( i + 1 ))
+	i=$(( i + 1 ))
 
 	sleep 1
 done
